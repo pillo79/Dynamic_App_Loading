5332694873a58d1a2bf7af10f7168e0f4a72f424
Luca Burelli <l.burelli@arduino.cc>
2023-06-26 18:24:37 +0200

    Add millis/micros/delay APIs

--- kernel/modules/zephyr_api/arduino.cpp
+++ kernel/modules/zephyr_api/arduino.cpp
@@ -1,8 +1,9 @@
 #include "Arduino.h"
 
-#if !USERLIB
+#if !CONFIG_USERLIB
 
 extern "C" {
+
 static inline void z_vrfy_pinMode(pin_size_t pinNumber, PinMode mode)
 {
 	return pinMode(pinNumber, mode);
@@ -18,14 +19,38 @@ static inline PinStatus z_vrfy_digitalRead(pin_size_t pinNumber)
 	return digitalRead(pinNumber);
 }
 
+static inline unsigned long z_vrfy_millis(void)
+{
+	return millis();
+}
+
+static inline unsigned long z_vrfy_micros(void)
+{
+	return micros();
+}
+
+static inline void z_vrfy_delay(unsigned long ms)
+{
+	delay(ms);
+}
+
+static inline void z_vrfy_delayMicroseconds(unsigned int us)
+{
+	delayMicroseconds(us);
+}
+
 #include <syscalls/pinMode_mrsh.c>
 #include <syscalls/digitalWrite_mrsh.c>
 #include <syscalls/digitalRead_mrsh.c>
+#include <syscalls/millis_mrsh.c>
+#include <syscalls/micros_mrsh.c>
+#include <syscalls/delay_mrsh.c>
+#include <syscalls/delayMicroseconds_mrsh.c>
+
 }
 
 #else
 
-#define __pinned_func
 #include <syscalls/Common.h>
 
 #endif
